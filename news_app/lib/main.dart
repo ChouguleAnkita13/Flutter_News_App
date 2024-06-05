422087fe6e65f33f6d35592c2355ea47c026cd22
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-05 15:12:45 +0530

    provider added

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/route/app_route.dart';
+import 'package:news_app/controller/news_provider.dart';
+import 'package:provider/provider.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +12,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: initialScreen,
-      debugShowCheckedModeBanner: false,
-      routes: appRoutes,
+    return ChangeNotifierProvider( // Use ChangeNotifierProvider to provide NewsProvider to the entire app
+      create: (context) => NewsProvider(), 
+      child: MaterialApp(
+        home: initialScreen, 
+        routes: appRoutes, 
+        debugShowCheckedModeBanner: false, // Disable the debug banner
+      ),
     );
   }
 }
