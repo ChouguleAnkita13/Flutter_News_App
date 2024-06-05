5fcf555a89d6ceafe39dbc846a048d11cd0fec63
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-05 15:33:17 +0530

    back button and icon added

--- news_app/lib/view/screens/bookmarked_news_screen.dart
+++ news_app/lib/view/screens/bookmarked_news_screen.dart
@@ -17,24 +17,80 @@ class BookmarkedNewsScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        leading: Image.asset(
+          "assets/images/logo.png",
+          height: 20,
+          width: 20,
+        ),
         title: Text(
-          'Bookmarked News', 
+          'Bookmarked News',
           style: GoogleFonts.poppins(
             fontWeight: FontWeight.w700,
             color: const Color.fromRGBO(0, 0, 0, 1),
             fontSize: 20,
           ),
         ),
+        automaticallyImplyLeading: false,
       ),
-      body: bookmarkedNews.isEmpty // Show different UI based on whether there are bookmarked news or not
+      body: bookmarkedNews.isEmpty
           ? Center(
-              child: Text(
-                'No bookmarked news', 
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w500,
-                  color: const Color.fromRGBO(0, 0, 0, 0.5),
-                  fontSize: 18,
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.bookmark_outline,
+                    size: 60,
+                    color: Color.fromRGBO(0, 0, 0, 0.5),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'No bookmarked news',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      color: const Color.fromRGBO(0, 0, 0, 0.5),
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                   GestureDetector(
+                    onTap: (){
+                          Navigator.of(context).pushNamed("/home");
+                        },
+                    child: Container(
+                      width: 300,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color.fromRGBO(255, 128, 134, 1),
+                            Color.fromRGBO(255, 58, 68, 1),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 3,
+                            offset:
+                                const Offset(0, 2), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Text(
+                        'Back to News',
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             )
           : ListView.builder(
@@ -46,7 +102,7 @@ class BookmarkedNewsScreen extends StatelessWidget {
                     Navigator.of(context).push(
                       MaterialPageRoute(
                         builder: (context) =>
-                            NewsDetailsScreen(detailNews: news), 
+                            NewsDetailsScreen(detailNews: news),
                       ),
                     );
                   },
@@ -59,16 +115,16 @@ class BookmarkedNewsScreen extends StatelessWidget {
                     child: Stack(
                       children: [
                         Image.network(
-                          news.imageUrl!, // Display the news image
+                          news.imageUrl!,
                           fit: BoxFit.cover,
                           width: double.infinity,
                           height: 150,
                         ),
                         Positioned.fill(
                           child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1), // Apply blur effect to the image
+                            filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
                             child: Container(
-                              color: Colors.black.withOpacity(0.1), // Background color for the blurred area
+                              color: Colors.black.withOpacity(0.1),
                             ),
                           ),
                         ),
@@ -82,7 +138,7 @@ class BookmarkedNewsScreen extends StatelessWidget {
                             decoration: BoxDecoration(
                               gradient: LinearGradient(
                                 colors: [
-                                  Colors.black.withOpacity(0.6), // Gradient color for text background
+                                  Colors.black.withOpacity(0.6),
                                   Colors.transparent,
                                 ],
                                 begin: Alignment.bottomCenter,
@@ -94,20 +150,21 @@ class BookmarkedNewsScreen extends StatelessWidget {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
-                                  news.title!, // Display the news title
+                                  news.title!,
                                   style: GoogleFonts.poppins(
                                     fontWeight: FontWeight.w700,
                                     color: Colors.white,
                                     fontSize: 16,
                                   ),
-                                  maxLines: 2, 
+                                  maxLines: 2,
                                   overflow: TextOverflow.ellipsis,
                                 ),
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: [
                                     Text(
-                                      news.author ?? 'Unknown', // Display the news author or 'Unknown' if author is null
+                                      news.author ?? 'Unknown',
                                       style: GoogleFonts.poppins(
                                         fontWeight: FontWeight.w500,
                                         color: Colors.white70,
@@ -115,7 +172,7 @@ class BookmarkedNewsScreen extends StatelessWidget {
                                       ),
                                     ),
                                     Text(
-                                      news.date!, // Display the news date
+                                      news.date!,
                                       style: GoogleFonts.poppins(
                                         fontWeight: FontWeight.w500,
                                         color: Colors.white70,
