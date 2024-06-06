734af2b076f31d5598fae0dfb257020476e09f5e
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-06 15:27:10 +0530

    minor changes

--- news_app/lib/controller/getnews.dart
+++ news_app/lib/controller/getnews.dart
@@ -5,15 +5,26 @@ import 'package:news_app/model/newsmodel.dart';
 
 // Function to fetch news data from the API based on the specified category
 
-Future<List<Data>> getNews(String category)async{
-  Uri url=Uri.parse("https://inshortsapi.vercel.app/news?category=$category");
+Future<List<Data>> getNews(String category) async {
+  try {
+    Uri url = Uri.parse("https://inshortsapi.vercel.app/news?category=$category");
 
-  http.Response response =await http.get(url);
+    http.Response response = await http.get(url);
 
-  Map<String,dynamic> responseData=json.decode(response.body);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseData = json.decode(response.body);
 
-  NewsModel newsModel=NewsModel(responseData);
-  log("${newsModel.category}");
+      NewsModel newsModel = NewsModel(responseData);
+      log("${newsModel.category}");
 
-  return newsModel.data!;
+      return newsModel.data ?? [];
+    } else {
+      log("Failed to load news: ${response.statusCode}");
+      return [];
+    }
+  } catch (e) {
+    // Handle any exceptions that occur during the request or parsing
+    log("An error occurred: $e");
+    return [];
+  }
 }
