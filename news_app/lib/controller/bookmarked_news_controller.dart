c29fe784355a451a065e1810b0cb65d88ee3d968
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-23 22:56:21 +0530

    adding db

--- news_app/lib/controller/bookmarked_news_controller.dart
+++ news_app/lib/controller/bookmarked_news_controller.dart
@@ -1,8 +1,19 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+dynamic database;
 
-
-
-
-
-
-
+Future getDatabase() async {
+  database = openDatabase(
+    join(await getDatabasesPath(), "bookmarkedDB.db"),
+    version: 1,
+    onCreate: (db, version) {
+      return db.execute('''Create table BMNews(
+    imgUrl Text,
+    title Text,
+    author Text,date Text
+    )
+    ''');
+    },
+  );
+}
